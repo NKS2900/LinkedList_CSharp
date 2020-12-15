af6662c0594bf9ab083f555e73aa4af5b4eb525f
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-15 19:51:45 +0530

    [NIJAM] Add:Creating simple LinkedList.

---
+++ LinkedList_CSharp/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace LinkedList_CSharp
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
--- LinkedList_CSharp/Program.cs
+++ LinkedList_CSharp/Program.cs
@@ -2,13 +2,16 @@
 
 namespace LinkedList_CSharp
 {
-
     class Program
     {
-
         static void Main(string[] args)
         {
             Console.WriteLine("***Welcome_To_LinkedListApp***");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
